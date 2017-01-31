4a34c7d2343fa8b68223326c82c71f8a341cfa8d
17012509 <17012509@e010m07.istic.univ-rennes1.fr>
2017-01-31 10:06:53 +0100

    Update
    Ajout des classes Dessin, Forme, Rectangle et ligne

--- model.js
+++ model.js
@@ -1,3 +1,32 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+function Drawing(){
+	
+}
+
+function Forme(){
+	var couleur, epaisseur;
+	
+	this.getCouleur = function(){
+		return this.couleur;
+	}.bind(this);
+	
+	this.getEpaisseur = function(){
+		return this.epaisseur;
+	}.bind(this);	
+}
+
+function Rectangle(couleur, epaisseur){
+	Forme.call(this, couleur, epaisseur);
+	
+	
+}
+Rectangle.prototype = new Forme();
+
+function Ligne(couleur, epaisseur){
+	Forme.call(this, couleur, epaisseur);
+	
+	
+}
+Ligne.prototype = new Forme();
